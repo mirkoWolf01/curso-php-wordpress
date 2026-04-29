ff9fa7e481758502db8d110f4057cd608eebd13f
mirko <mirkowolf46@gmail.com>
2026-04-29 18:42:20 -0300

    testeo inicial

---
+++ test/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Index</h1>
+    <h2>Lorem ipsum</h2>
+
+    <nav>
+        <p>Sector de navegacion</p>
+        <ol>
+            <li>Elemento de lista</li>
+        </ol>
+
+    </nav>
+
+    <?php
+    echo "<h2>Introduccion a PHP</h2>";
+    echo "Hola mundo! <br>";
+    echo "Estamos aprendiendo PHP! <br>";
+    ?>
+
+
+</body>
+
+</html>
